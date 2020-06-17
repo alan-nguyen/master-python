bcba3a9badc245addf6df8753a9a4bb9629bb67e
Alan Nguyen <tuan8101@gmail.com>
2020-06-17 17:30:47 -0600

    Check if size > 0 and decrease size

--- linear_data_structure/03_stacks/stack.py
+++ linear_data_structure/03_stacks/stack.py
@@ -7,15 +7,23 @@ class Stack:
         self.limit = limit
         self.size = 0
 
-    def peek(self):
-        return self.top_item.get_value()
-
     def push(self, value):
         item = Node(value)
         item.set_next_node(self.top_item)
         self.top_item = item
 
     def pop(self):
-        item_to_remove = self.top_item
-        self.top_item = item_to_remove.get_next_node()
-        return item_to_remove.get_value()
+        if self.size > 0:
+            item_to_remove = self.top_item
+            self.top_item = item_to_remove.get_next_node()
+            # Decrement the stack size here:
+            self.size -= 1
+            return item_to_remove.get_value()
+        else:
+            print('Stack is empty')
+
+    def peek(self):
+        if self.size > 0:
+            return self.top_item.get_value()
+        else:
+            print('Stack is empty')

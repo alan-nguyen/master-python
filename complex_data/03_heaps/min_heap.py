09a3a55bd945e308b3148c5021c6d37dd8f07fab
Alan Nguyen <tuan8101@gmail.com>
2020-06-18 17:00:18 -0600

    Update heapify_up to swap values

--- complex_data/03_heaps/min_heap.py
+++ complex_data/03_heaps/min_heap.py
@@ -8,9 +8,6 @@ class MinHeap:
     self.count += 1
     self.heap_list.append(element)
     self.heapify_up()
-  
-  def heapify_up(self):
-    print("Restoring the heap property...")
 
   # HEAP HELPER METHODS
   def parent_idx(self, idx):
@@ -21,3 +18,21 @@ class MinHeap:
 
   def right_child_idx(self, idx):
     return idx * 2 + 1
+
+  def heapify_up(self):
+    print("Heapifying up")
+    # start at the last element of the list
+    # while there's a parent element available:
+        # if the parent element is greater:
+          # swap the elements
+        # set the target element index to be the parent's index
+    idx = self.count
+    while self.parent_idx(idx) > 0:
+      child = self.heap_list[idx]
+      parent = self.heap_list[self.parent_idx(idx)]
+      if parent > child:
+        print(f"swapping {parent} with {child} ")
+        self.heap_list[self.parent_idx(idx)] = child
+        self.heap_list[idx] = parent
+      idx = self.parent_idx(idx)
+    print(f"Heap Restored {self.heap_list}")

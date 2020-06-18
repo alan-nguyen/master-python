12f52ae69716fdc722cc7763d24b2a29902688e0
Alan Nguyen <tuan8101@gmail.com>
2020-06-18 17:34:29 -0600

    Test get_smaller_child_idx method

--- complex_data/03_heaps/script.py
+++ complex_data/03_heaps/script.py
@@ -18,10 +18,29 @@ min_heap = MinHeap()
 # # test it out, if the minimum number at index 1
 # print(min_heap.heap_list)
 
-# add elements
-min_heap.add(7)
-min_heap.add(12)
-min_heap.add(42)
+# # add elements
+# min_heap.add(7)
+# min_heap.add(12)
+# min_heap.add(42)
 
-# remove minimum element
-print(min_heap.retrieve_min())
+# # remove minimum element
+# print(min_heap.retrieve_min())
+
+# set internal list for testing purposes...
+min_heap.heap_list = [None, 10, 13, 21, 61, 22, 23, 99]
+min_heap.count = 7
+
+print("The smaller child of index 1 is: ")
+smaller_child_of_idx_1 = min_heap.get_smaller_child_idx(1)
+smaller_child_element = min_heap.heap_list[smaller_child_of_idx_1]
+print(smaller_child_element)
+
+print("The smaller child of index 2 is: ")
+smaller_child_of_idx_2 = min_heap.get_smaller_child_idx(2)
+smaller_child_element = min_heap.heap_list[smaller_child_of_idx_2]
+print(smaller_child_element)
+
+print("The smaller child of index 3 is: ")
+smaller_child_of_idx_3 = min_heap.get_smaller_child_idx(3)
+smaller_child_element = min_heap.heap_list[smaller_child_of_idx_3]
+print(smaller_child_element)

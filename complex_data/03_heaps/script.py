6d7a8daf7fcd286f9e30c7f074ac8b7bf9303d0a
Alan Nguyen <tuan8101@gmail.com>
2020-06-18 17:12:55 -0600

    Test retrieve_min method

--- complex_data/03_heaps/script.py
+++ complex_data/03_heaps/script.py
@@ -4,16 +4,24 @@ from min_heap import MinHeap
 from random import randrange
 
 min_heap = MinHeap()
-print(min_heap.heap_list)
+# print(min_heap.heap_list)
 
 # testing out .add()
 # min_heap.add(42)
 # print(min_heap.heap_list)
 
-# populate min_heap with random numbers
-random_nums = [randrange(1, 101) for n in range(6)]
-for el in random_nums:
-  min_heap.add(el)
+# # populate min_heap with random numbers
+# random_nums = [randrange(1, 101) for n in range(6)]
+# for el in random_nums:
+#   min_heap.add(el)
 
-# test it out, if the minimum number at index 1
-print(min_heap.heap_list)
+# # test it out, if the minimum number at index 1
+# print(min_heap.heap_list)
+
+# add elements
+min_heap.add(7)
+min_heap.add(12)
+min_heap.add(42)
+
+# remove minimum element
+print(min_heap.retrieve_min())

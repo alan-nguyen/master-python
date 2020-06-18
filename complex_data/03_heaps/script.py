f4e196ae750d25b10b35f5d3c4abecb866f9f279
Alan Nguyen <tuan8101@gmail.com>
2020-06-18 17:47:17 -0600

    Test heapify_down method

--- complex_data/03_heaps/script.py
+++ complex_data/03_heaps/script.py
@@ -26,21 +26,31 @@ min_heap = MinHeap()
 # # remove minimum element
 # print(min_heap.retrieve_min())
 
+# ----------- Test get_smaller_child_idx method -------------
+# # set internal list for testing purposes...
+# min_heap.heap_list = [None, 10, 13, 21, 61, 22, 23, 99]
+# min_heap.count = 7
+
+# print("The smaller child of index 1 is: ")
+# smaller_child_of_idx_1 = min_heap.get_smaller_child_idx(1)
+# smaller_child_element = min_heap.heap_list[smaller_child_of_idx_1]
+# print(smaller_child_element)
+
+# print("The smaller child of index 2 is: ")
+# smaller_child_of_idx_2 = min_heap.get_smaller_child_idx(2)
+# smaller_child_element = min_heap.heap_list[smaller_child_of_idx_2]
+# print(smaller_child_element)
+
+# print("The smaller child of index 3 is: ")
+# smaller_child_of_idx_3 = min_heap.get_smaller_child_idx(3)
+# smaller_child_element = min_heap.heap_list[smaller_child_of_idx_3]
+# print(smaller_child_element)
+
+# ------- Test Heapify_down --------------------
 # set internal list for testing purposes...
 min_heap.heap_list = [None, 10, 13, 21, 61, 22, 23, 99]
 min_heap.count = 7
 
-print("The smaller child of index 1 is: ")
-smaller_child_of_idx_1 = min_heap.get_smaller_child_idx(1)
-smaller_child_element = min_heap.heap_list[smaller_child_of_idx_1]
-print(smaller_child_element)
-
-print("The smaller child of index 2 is: ")
-smaller_child_of_idx_2 = min_heap.get_smaller_child_idx(2)
-smaller_child_element = min_heap.heap_list[smaller_child_of_idx_2]
-print(smaller_child_element)
-
-print("The smaller child of index 3 is: ")
-smaller_child_of_idx_3 = min_heap.get_smaller_child_idx(3)
-smaller_child_element = min_heap.heap_list[smaller_child_of_idx_3]
-print(smaller_child_element)
+while len(min_heap.heap_list) != 1:
+  print(min_heap.heap_list)
+  min_heap.retrieve_min()

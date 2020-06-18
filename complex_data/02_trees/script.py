419f7e7b04ddc50b8238cf73f45bac88e1271b6c
Alan Nguyen <tuan8101@gmail.com>
2020-06-18 09:31:27 -0600

    Add children list

--- complex_data/02_trees/script.py
+++ complex_data/02_trees/script.py
@@ -2,8 +2,13 @@ class TreeNode:
   def __init__(self, value):
     print('initializing node...')
     self.value = value
+    self.children = []
 
   def add_child(self, child_node):
     print('Adding ' + child_node.value)
     self.children.append(child_node)
 
+# Create an instance
+root = TreeNode("I am Root")
+child = TreeNode("A wee sappling")
+root.add_child(child)

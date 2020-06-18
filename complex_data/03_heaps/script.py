970bcad1b57883e9c987d6e94e0364cca1392ebf
Alan Nguyen <tuan8101@gmail.com>
2020-06-18 17:04:00 -0600

    Test heapify_up

--- complex_data/03_heaps/script.py
+++ complex_data/03_heaps/script.py
@@ -1,9 +1,19 @@
 # import heap class
 from min_heap import MinHeap 
+# import random number generator
+from random import randrange
 
 min_heap = MinHeap()
 print(min_heap.heap_list)
 
 # testing out .add()
-min_heap.add(42)
+# min_heap.add(42)
+# print(min_heap.heap_list)
+
+# populate min_heap with random numbers
+random_nums = [randrange(1, 101) for n in range(6)]
+for el in random_nums:
+  min_heap.add(el)
+
+# test it out, if the minimum number at index 1
 print(min_heap.heap_list)

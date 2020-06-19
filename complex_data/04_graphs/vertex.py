f178d880621899e5f06244e98924c8462de748e4
Alan Nguyen <tuan8101@gmail.com>
2020-06-18 20:49:04 -0600

    Test add_edge method

--- complex_data/04_graphs/vertex.py
+++ complex_data/04_graphs/vertex.py
@@ -13,4 +13,11 @@ class Vertex:
 # # Create an instance
 # station = Vertex("Cronk")
   
+grand_central = Vertex('Grand Central Station')
+forty_second_street = Vertex('42nd Street Station')
 
+print(grand_central.get_edges())
+
+# call .add_edge()
+grand_central.add_edge(forty_second_street.value)
+print(grand_central.get_edges())

a6ab678db8fe0d8b1b99ad312dac756deaeec030
Alan Nguyen <tuan8101@gmail.com>
2020-06-19 04:43:48 -0600

    Add test cases and runtime for sort_linked_list

--- asymptotic_notation/linkedlist/script.py
+++ asymptotic_notation/linkedlist/script.py
@@ -52,9 +52,36 @@ def sort_linked_list(linked_list):
   print("\n---------------------------")
   print("The original linked list is:\n{0}".format(linked_list.stringify_list()))
   new_linked_list = LinkedList()
-  #Write Code Here!
   while linked_list.get_head_node():
     max_val = find_max(linked_list)
     new_linked_list.insert_beginning(max_val)
     linked_list.remove_node(max_val)
   return new_linked_list
+
+#Test Cases for sort_linked_list
+ll = LinkedList("Z")
+ll.insert_beginning("C")
+ll.insert_beginning("Q")
+ll.insert_beginning("A")
+print("The sorted linked list is:\n{0}".format(sort_linked_list(ll).stringify_list()))
+
+ll_2 = LinkedList(1)
+ll_2.insert_beginning(4)
+ll_2.insert_beginning(18)
+ll_2.insert_beginning(2)
+ll_2.insert_beginning(3)
+ll_2.insert_beginning(7)
+print("The sorted linked list is:\n{0}".format(sort_linked_list(ll_2).stringify_list()))
+
+ll_3 = LinkedList(-11)
+ll_3.insert_beginning(44)
+ll_3.insert_beginning(118)
+ll_3.insert_beginning(1000)
+ll_3.insert_beginning(23)
+ll_3.insert_beginning(-92)
+print("The sorted linked list is:\n{0}".format(sort_linked_list(ll_3).stringify_list()))
+
+#Runtime for sort_linked_list
+runtime = "N^2"
+print("The runtime of sort_linked_list is O({0})\n\n".format(runtime))
+# END of sort_linked_list

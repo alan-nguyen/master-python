4f2c99b37e0da469eb90d8eb36a93c33c72fe68f
Alan Nguyen <tuan8101@gmail.com>
2020-06-18 20:48:46 -0600

    Add add_edge method

--- complex_data/04_graphs/vertex.py
+++ complex_data/04_graphs/vertex.py
@@ -3,8 +3,14 @@ class Vertex:
     self.value = value
     self.edges = {}
   
+  def add_edge(self, vertex):
+    print(f"Adding edge to {vertex}")
+    self.edges[vertex] = True
+
   def get_edges(self):
     return list(self.edges.keys())
 
-# Create an instance
-station = Vertex("Cronk")
+# # Create an instance
+# station = Vertex("Cronk")
+  
+

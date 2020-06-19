d580c28f7ff39a2719b22914060c06dac2b7e816
Alan Nguyen <tuan8101@gmail.com>
2020-06-19 12:16:07 -0600

    Create power_set using recursion

--- recursion/power_set.py
+++ recursion/power_set.py
@@ -11,3 +11,21 @@ def power_set(set):
   return result
 
 print(power_set(['a', 'b', 'c']))
+
+
+# ------ Power set using recursion ----------------
+def power_set_rec(my_list):
+    # base case: an empty list
+    if len(my_list) == 0:
+        return [[]]
+    # recursive step: subsets without first element
+    power_set_without_first = power_set_rec(my_list[1:])
+    # subsets with first element
+    with_first = [ [my_list[0]] + rest for rest in power_set_without_first ]
+    # return combination of the two
+    return with_first + power_set_without_first
+  
+universities = ['MIT', 'UCLA', 'Stanford', 'NYU']
+power_set_of_universities = power_set_rec(universities)
+for set in power_set_of_universities:
+  print(set)

17ae171701dfd341fe671a496fa90d3ba75e1867
Alan Nguyen <tuan8101@gmail.com>
2020-06-19 04:27:02 -0600

    Add test cases and runtime

--- asymptotic_notation/linkedlist/script.py
+++ asymptotic_notation/linkedlist/script.py
@@ -11,5 +11,30 @@ def find_max(linked_list):
       max_value = current.get_value()
   return max_value
   
-  
 
+#Test Cases
+ll = LinkedList(6)
+ll.insert_beginning(32)
+ll.insert_beginning(-12)
+ll.insert_beginning(48)
+ll.insert_beginning(2)
+ll.insert_beginning(1)
+print("The maximum value in this linked list is {0}\n".format(find_max(ll)))
+
+ll_2 = LinkedList(60)
+ll_2.insert_beginning(12)
+ll_2.insert_beginning(22)
+ll_2.insert_beginning(-10)
+print("The maximum value in this linked list is {0}\n".format(find_max(ll_2)))
+
+ll_3 = LinkedList("A")
+ll_3.insert_beginning("X")
+ll_3.insert_beginning("V")
+ll_3.insert_beginning("L")
+ll_3.insert_beginning("D")
+ll_3.insert_beginning("Q")
+print("The maximum value in this linked list is {0}\n".format(find_max(ll_3)))
+
+#Runtime
+runtime = "N"
+print("The runtime of find_max is O({0})".format(runtime))

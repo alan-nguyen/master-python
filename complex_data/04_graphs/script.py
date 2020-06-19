d0821eda8fa3bc4161354bac6b16e7f5bec070ec
Alan Nguyen <tuan8101@gmail.com>
2020-06-18 21:43:06 -0600

    Test find_path for all station

--- complex_data/04_graphs/script.py
+++ complex_data/04_graphs/script.py
@@ -35,20 +35,48 @@ from graph import Graph
 # print(peel.edges)
 
 # -------- Test find_path ------------
-undirected_railway = Graph()
+# undirected_railway = Graph()
+
+# callan_station = Vertex('callan')
+# peel_station = Vertex('peel')
+# ulfstead_station = Vertex('ulfstead')
+# harwick_station = Vertex('harwick')
+
+# undirected_railway.add_vertex(callan_station)
+# undirected_railway.add_vertex(peel_station)
+# undirected_railway.add_vertex(harwick_station)
+# undirected_railway.add_vertex(ulfstead_station)
+
+# undirected_railway.add_edge(peel_station, harwick_station)
+# undirected_railway.add_edge(peel_station, callan_station)
+
+
+# undirected_railway.find_path('harwick', 'callan')
+
+# --------- Test find_path for all station ---------- 
+no_path_exists = True
+
+
+directed_railway = Graph(True)
 
 callan_station = Vertex('callan')
 peel_station = Vertex('peel')
 ulfstead_station = Vertex('ulfstead')
 harwick_station = Vertex('harwick')
 
-undirected_railway.add_vertex(callan_station)
-undirected_railway.add_vertex(peel_station)
-undirected_railway.add_vertex(harwick_station)
-undirected_railway.add_vertex(ulfstead_station)
+directed_railway.add_vertex(callan_station)
+directed_railway.add_vertex(peel_station)
+directed_railway.add_vertex(harwick_station)
+directed_railway.add_vertex(ulfstead_station)
 
-undirected_railway.add_edge(peel_station, harwick_station)
-undirected_railway.add_edge(peel_station, callan_station)
+directed_railway.add_edge(harwick_station, peel_station)
+directed_railway.add_edge(peel_station, callan_station)
+path_exists = directed_railway.find_path('harwick', 'harwick')
+print(path_exists)
 
-
-undirected_railway.find_path('harwick', 'callan')
+print("\n\n\nFinding path from harwick to callan\n")
+new_path_exists = directed_railway.find_path('harwick', 'callan')
+print(new_path_exists)
+print("\n\nTrying to find path from harwick to ulfstead\n")
+no_path_exists = directed_railway.find_path('harwick', 'ulfstead')
+print(no_path_exists)

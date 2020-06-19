667a00b85796e4a2d6b0a6629ab6071cf6f86da3
Alan Nguyen <tuan8101@gmail.com>
2020-06-18 21:51:35 -0600

    Test refractor find_path

--- complex_data/04_graphs/script.py
+++ complex_data/04_graphs/script.py
@@ -54,29 +54,54 @@ from graph import Graph
 # undirected_railway.find_path('harwick', 'callan')
 
 # --------- Test find_path for all station ---------- 
-no_path_exists = True
+# no_path_exists = True
 
 
-directed_railway = Graph(True)
+# directed_railway = Graph(True)
 
-callan_station = Vertex('callan')
-peel_station = Vertex('peel')
-ulfstead_station = Vertex('ulfstead')
-harwick_station = Vertex('harwick')
+# callan_station = Vertex('callan')
+# peel_station = Vertex('peel')
+# ulfstead_station = Vertex('ulfstead')
+# harwick_station = Vertex('harwick')
 
-directed_railway.add_vertex(callan_station)
-directed_railway.add_vertex(peel_station)
-directed_railway.add_vertex(harwick_station)
-directed_railway.add_vertex(ulfstead_station)
+# directed_railway.add_vertex(callan_station)
+# directed_railway.add_vertex(peel_station)
+# directed_railway.add_vertex(harwick_station)
+# directed_railway.add_vertex(ulfstead_station)
 
-directed_railway.add_edge(harwick_station, peel_station)
-directed_railway.add_edge(peel_station, callan_station)
-path_exists = directed_railway.find_path('harwick', 'harwick')
-print(path_exists)
+# directed_railway.add_edge(harwick_station, peel_station)
+# directed_railway.add_edge(peel_station, callan_station)
+# path_exists = directed_railway.find_path('harwick', 'harwick')
+# print(path_exists)
 
-print("\n\n\nFinding path from harwick to callan\n")
-new_path_exists = directed_railway.find_path('harwick', 'callan')
-print(new_path_exists)
-print("\n\nTrying to find path from harwick to ulfstead\n")
-no_path_exists = directed_railway.find_path('harwick', 'ulfstead')
-print(no_path_exists)
+# print("\n\n\nFinding path from harwick to callan\n")
+# new_path_exists = directed_railway.find_path('harwick', 'callan')
+# print(new_path_exists)
+# print("\n\nTrying to find path from harwick to ulfstead\n")
+# no_path_exists = directed_railway.find_path('harwick', 'ulfstead')
+# print(no_path_exists)
+
+# ------- Test refractor find_path ------------
+railway = Graph()
+
+callan = Vertex('callan')
+peel = Vertex('peel')
+ulfstead = Vertex('ulfstead')
+harwick = Vertex('harwick')
+
+railway.add_vertex(callan)
+railway.add_vertex(peel)
+railway.add_vertex(harwick)
+railway.add_vertex(ulfstead)
+
+railway.add_edge(peel, harwick)
+railway.add_edge(harwick, callan)
+railway.add_edge(callan, peel)
+
+peel_to_ulfstead_path_exists = railway.find_path('peel', 'ulfstead')
+harwick_to_peel_path_exists = railway.find_path('harwick', 'peel')
+
+print("A path exists between peel and ulfstead:")
+print(peel_to_ulfstead_path_exists)
+print("A path exists between harwick and peel:")
+print(harwick_to_peel_path_exists)

3fe743b500d16c669d9622f5c5ce215a795f0563
Alan Nguyen <tuan8101@gmail.com>
2020-06-19 04:51:21 -0600

    Update find_max

--- asymptotic_notation/linkedlist/script.py
+++ asymptotic_notation/linkedlist/script.py
@@ -9,8 +9,8 @@ from linkedlist import LinkedList
 
 # - return maximum
 def find_max(linked_list):
-  print("--------------------------")
-  print("Finding the maximum value of:\n{0}".format(linked_list.stringify_list()))
+  # print("--------------------------")
+  # print("Finding the maximum value of:\n{0}".format(linked_list.stringify_list()))
   current = linked_list.get_head_node()
   max_value = current.get_value()
   while current.get_next_node():

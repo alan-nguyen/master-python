b8cd355cab9e9851fb9dedf3f9ee39d47b24eb68
Alan Nguyen <tuan8101@gmail.com>
2020-06-19 05:28:47 -0600

    Fix typo

--- asymptotic_notation/hashmap_linkedlist/linkedlist.py
+++ asymptotic_notation/hashmap_linkedlist/linkedlist.py
@@ -22,7 +22,7 @@ class LinkedList:
   
   def remove_node(self, value_to_remove):
     current_node = self.head_node
-    if current_node.get_value() == node_to_remove:
+    if current_node.get_value() == value_to_remove:
       self.head_node = current_node.get_next_node()
     else:
       while current_node:

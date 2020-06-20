408696f6fd8e361d89e44e01de0f3aca4c21945e
Alan Nguyen <tuan8101@gmail.com>
2020-06-20 14:18:03 -0600

    Call quicksort on the left and right sub-lists

--- sort_algorithms/quick_sort.py
+++ sort_algorithms/quick_sort.py
@@ -1,23 +1,31 @@
-# use randrange to produce a random index
-from random import randrange
-
+from random import randrange, shuffle 
 def quicksort(list, start, end):
+  # this portion of listay has been sorted
   if start >= end:
-    return list
-	# Define pivot variables 
-  pivot_idx = randrange(start, end)
+    return
+
+  # select random element to be pivot
+  pivot_idx = randrange(start, end + 1)
   pivot_element = list[pivot_idx]
-  # Swap the elements in list 
+
+  # swap random element with last element in sub-listay
   list[end], list[pivot_idx] = list[pivot_idx], list[end]
-  # Create the lesser_than_pointer
-  lesser_than_pointer = start
-  for idx in range(start, end):
-    if list[idx] < pivot_element:
-      list[lesser_than_pointer], list[idx] = list[idx], list[lesser_than_pointer]
-      lesser_than_pointer += 1
-  # After the loop is finished...
-  # swap pivot with value at lesser_than_pointer
-  list[end], list[lesser_than_pointer] = list[lesser_than_pointer], list[end]
-  print(list[start])
-  start += 1
-  return quicksort(list, start, end)
+
+  # tracks all elements which should be to left (lesser than) pivot
+  less_than_pointer = start
+  
+  for i in range(start, end):
+    # we found an element out of place
+    if list[i] < pivot_element:
+      # swap element to the right-most portion of lesser elements
+      list[i], list[less_than_pointer] = list[less_than_pointer], list[i]
+      # tally that we have one more lesser element
+      less_than_pointer += 1
+  # move pivot element to the right-most portion of lesser elements
+  list[end], list[less_than_pointer] = list[less_than_pointer], list[end]
+  
+  # Call quicksort on the "left" and "right" sub-lists
+  quicksort(list, start, less_than_pointer - 1)
+  quicksort(list, less_than_pointer + 1, end)
+  
+  

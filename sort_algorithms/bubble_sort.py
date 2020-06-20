08b876609a675a2c35900ebfafb33bc2a253ac38
Alan Nguyen <tuan8101@gmail.com>
2020-06-20 07:45:10 -0600

    Create bubble_sort function

--- sort_algorithms/bubble_sort.py
+++ sort_algorithms/bubble_sort.py
@@ -14,3 +14,15 @@ def bubble_sort_unoptimized(arr):
 
   print("PRE-OPTIMIZED ITERATION COUNT: {0}".format(iteration_count))
 
+def bubble_sort(arr):
+  iteration_count = 0
+  for i in range(len(arr)):
+    # iterate through unplaced elements
+    for idx in range(len(arr) - i - 1):
+      iteration_count += 1
+      if arr[idx] > arr[idx + 1]:
+        # replacement for swap function
+        arr[idx], arr[idx + 1] = arr[idx + 1], arr[idx]
+        
+  print("POST-OPTIMIZED ITERATION COUNT: {0}".format(iteration_count))
+

24f7dc5d4a17ddd8469ae6ff53f9e80ebc02e723
Alan Nguyen <tuan8101@gmail.com>
2020-06-20 13:43:49 -0600

    Add partition

--- sort_algorithms/quick_sort.py
+++ sort_algorithms/quick_sort.py
@@ -4,9 +4,20 @@ from random import randrange
 def quicksort(list, start, end):
   if start >= end:
     return list
-	# Define your pivot variables below
+	# Define pivot variables 
   pivot_idx = randrange(start, end)
   pivot_element = list[pivot_idx]
-  # Swap the elements in list below
+  # Swap the elements in list 
   list[end], list[pivot_idx] = list[pivot_idx], list[end]
-  
+  # Create the lesser_than_pointer
+  lesser_than_pointer = start
+  for idx in range(start, end):
+    if list[idx] < pivot_element:
+      list[lesser_than_pointer], list[idx] = list[idx], list[lesser_than_pointer]
+      lesser_than_pointer += 1
+  # After the loop is finished...
+  # swap pivot with value at lesser_than_pointer
+  list[end], list[lesser_than_pointer] = list[lesser_than_pointer], list[end]
+  print(list[start])
+  start += 1
+  return quicksort(list, start, end)

5992cbfa15f9f2d069c68b2f529ca027019c575b
Alan Nguyen <tuan8101@gmail.com>
2020-06-20 17:59:45 -0600

    Iterate through all exponents

--- sort_algorithms/radix_sort.py
+++ sort_algorithms/radix_sort.py
@@ -1,21 +1,24 @@
 def radix_sort(to_be_sorted):
   maximum_value = max(to_be_sorted)
   max_exponent = len(str(maximum_value))
-  return max_exponent
-  
   # Create copy of to_be_sorted 
   being_sorted = to_be_sorted[:]
-  digits = [[] for digit in range(10)]
 
-  # Bucket numbers
-  for number in being_sorted:
-    number_as_a_string = str(number)
-    digit = number_as_a_string[-1]
-    digit = int(digit)
-    digits[digit].append(number)
+  # Loop over all exponents
+  for exponent in range(max_exponent):
+    index = - (exponent + 1)
+    digits = [[] for digit in range(10)]
 
-  # reassign being_sorted
-  being_sorted = []
-  for numeral in digits:
-    being_sorted.extend(numeral)
+    # Bucket numbers
+    for number in being_sorted:
+      number_as_a_string = str(number)
+      digit = number_as_a_string[index]
+      digit = int(digit)
+      digits[digit].append(number)
+
+    # Reassign being_sorted
+    being_sorted = []
+    for numeral in digits:
+      being_sorted.extend(numeral)
+
   return being_sorted

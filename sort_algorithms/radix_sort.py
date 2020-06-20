6ec20d9065f9928a3825459738aaa1c5c6c08fc1
Alan Nguyen <tuan8101@gmail.com>
2020-06-20 17:41:01 -0600

    Set up for sorting

--- sort_algorithms/radix_sort.py
+++ sort_algorithms/radix_sort.py
@@ -2,3 +2,8 @@ def radix_sort(to_be_sorted):
   maximum_value = max(to_be_sorted)
   max_exponent = len(str(maximum_value))
   return max_exponent
+  
+  # create copy of to_be_sorted 
+  being_sorted = to_be_sorted[:]
+  digits = [[] for digit in range(10)]
+  return digits

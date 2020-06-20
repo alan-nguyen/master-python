4416936246cf1811a4ae7a582e1bf9920178d4c5
Alan Nguyen <tuan8101@gmail.com>
2020-06-20 07:44:54 -0600

    Create bubble_sort_unoptimized function

--- sort_algorithms/bubble_sort.py
+++ sort_algorithms/bubble_sort.py
@@ -4,3 +4,13 @@ def swap(arr, index_1, index_2):
   arr[index_1] = arr[index_2]
   arr[index_2] = temp
 
+def bubble_sort_unoptimized(arr):
+  iteration_count = 0
+  for el in arr:
+    for index in range(len(arr) - 1):
+      iteration_count += 1
+      if arr[index] > arr[index + 1]:
+        swap(arr, index, index + 1)
+
+  print("PRE-OPTIMIZED ITERATION COUNT: {0}".format(iteration_count))
+

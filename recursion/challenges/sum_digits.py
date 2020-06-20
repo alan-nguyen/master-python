32d6610a580411329398881bfcca220c9d3d2d4c
Alan Nguyen <tuan8101@gmail.com>
2020-06-19 22:38:02 -0600

    Add runtime

--- recursion/challenges/sum_digits.py
+++ recursion/challenges/sum_digits.py
@@ -9,6 +9,7 @@ def sum_digits(n):
     return last_digit + sum_digits(n // 10)
 
 # Using iteration
+## Linear - O(N), where "N" is the number of digits in the number
 # def sum_digits(n):
 #   if n < 0:
 #     ValueError("Inputs 0 or greater only!")

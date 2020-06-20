6eb691f11e44ab22a4703b9b7fe3f4329c0333a6
Alan Nguyen <tuan8101@gmail.com>
2020-06-20 08:31:11 -0600

    Add merge function

--- sort_algorithms/merge_sort.py
+++ sort_algorithms/merge_sort.py
@@ -1,7 +1,32 @@
 def merge_sort(items):
   if len(items) <= 1:
     return items
+
   middle_index = len(items) // 2
   left_split = items[:middle_index]
   right_split = items[middle_index:]
-  return middle_index, left_split, right_split
+
+# Helper function
+def merge(left, right):
+  result = []
+
+  # Loop until left or right is empty
+  while (left and right):
+    if left[0] < right[0]:
+      result.append(left[0]) 
+      left.pop(0)
+    else:
+      result.append(right[0])
+      right.pop(0)
+
+  # Merge the remain part which requires no comparision
+  if left:
+    result += left
+  if right:
+    result += right
+
+  return result
+
+
+
+

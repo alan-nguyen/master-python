1328f21d68fa9e45fb53a45096be3d2451a28f9c
Alan Nguyen <tuan8101@gmail.com>
2020-06-21 15:19:55 -0600

    Loop through each vertex in queue

--- graph_search/bfs.py
+++ graph_search/bfs.py
@@ -3,5 +3,14 @@ def bfs(graph, start_vertex, target_value):
   vertex_and_path = [start_vertex, path]
   bfs_queue = [vertex_and_path]
   visited = set()
-
-print(bfs(None, "lava!", None))
+  
+  # loop until queue is empty
+  while bfs_queue:
+    current_vertex, path = bfs_queue.pop(0)
+    visited.add(current_vertex)
+    for neighbor in graph[current_vertex]:
+      if neighbor not in visited:
+        if neighbor is target_value:
+          return path + [neighbor]
+        else:
+          bfs_queue.append([neighbor, path + [neighbor]])

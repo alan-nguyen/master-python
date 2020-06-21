4fd6312f0567814f225b87a089ff23fb2bed24fd
Alan Nguyen <tuan8101@gmail.com>
2020-06-20 19:11:27 -0600

    Add base case 2

--- search_algorithms/binary_search.py
+++ search_algorithms/binary_search.py
@@ -1,6 +1,10 @@
 def binary_search(sorted_list, target):
+  # Base case list is empty
   if sorted_list == []:
     return "value not found"
   mid_idx = len(sorted_list) // 2
   mid_val = sorted_list[mid_idx]
-  return mid_idx, mid_val
+  
+  # Base case found at first search
+  if mid_val == target:
+    return mid_idx

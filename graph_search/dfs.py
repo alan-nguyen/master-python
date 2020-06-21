1dd0c18a087bc661d2f8670a57ca5751c5bb5f98
Alan Nguyen <tuan8101@gmail.com>
2020-06-21 14:56:04 -0600

    Add recurise steps

--- graph_search/dfs.py
+++ graph_search/dfs.py
@@ -7,3 +7,9 @@ def dfs(graph, current_vertex, target_value, visited=None):
   if current_vertex == target_value:
     return visited
 	
+  # Add recursive  
+  for neighbor in graph[current_vertex]:
+    if neighbor not in visited:
+      path = dfs(graph, neighbor, target_value, visited)      
+      if path:
+        return path

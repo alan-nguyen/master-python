d0e74455baac541af7a43ab65c3ee74d6358dd0d
Alan Nguyen <tuan8101@gmail.com>
2020-06-20 18:54:47 -0600

    Add test case

--- search_algorithms/linear_search.py
+++ search_algorithms/linear_search.py
@@ -8,3 +8,10 @@ def linear_search(search_list, target_value):
     return matches
   else:
     raise ValueError("{0} not in list".format(target_value))
+
+# Test case for duplicating 
+tour_locations = [ "New York City", "Los Angeles", "Bangkok", "Istanbul", "London", "New York City", "Toronto"]
+target_city = "New York City"
+
+tour_stops = linear_search(tour_locations, target_city)
+print(tour_stops)

e5452c644494b8765ed7d4e88d8f550329f01aec
Alan Nguyen <tuan8101@gmail.com>
2020-06-23 09:36:58 -0600

    Fix bug

--- advanced_python/lambda/contains_a.py
+++ advanced_python/lambda/contains_a.py
@@ -1,5 +1,7 @@
 contains_a = lambda word: "a" in word
-print contains_a("banana")
-print contains_a("apple")
-print contains_a("cherry")
+
+print(contains_a("banana"))
+print(contains_a("apple"))
+print(contains_a("cherry"))
+
 

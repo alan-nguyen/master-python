df48b76752219a12cedfda67c85965379b1d639a
Alan Nguyen <tuan8101@gmail.com>
2020-06-24 23:17:13 -0600

    Create change

---
+++ 004_change.py
@@ -0,0 +1,13 @@
+def change(amount):
+    d = amount % 5
+    if d == 0:
+        return [5] * int(amount/5)
+    elif d == 1:
+        return [7] * 3 + [5] * int((amount-21)/5)
+    elif d == 2:
+        return [7] + [5] * int((amount-7)/5)
+    elif d == 3:
+        return [7] * 4 + [5] * int((amount-28)/5)
+    elif d == 4:
+        return [7] * 2 + [5] * int((amount-14)/5)
+

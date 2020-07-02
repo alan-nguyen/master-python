c82e94c89503a8042542f0d57341f8d7033251eb
Alan Nguyen <tuan8101@gmail.com>
2020-07-01 22:11:55 -0600

    Create find_the_best_dice function

--- count_wins.py
+++ count_wins.py
@@ -11,13 +11,34 @@ def count_wins(dice1, dice2):
   
     return (dice1_wins, dice2_wins)
 
-# Test cases
-dice1 = [1, 2, 3, 4, 5, 6]
-dice2 = [1, 2, 3, 4, 5, 6]
-# Expect 15, 15
-print(count_wins(dice1, dice2)) 
+# # Test cases
+# dice1 = [1, 2, 3, 4, 5, 6]
+# dice2 = [1, 2, 3, 4, 5, 6]
+# # Expect 15, 15
+# print(count_wins(dice1, dice2)) 
 
-dice1 = [1, 1, 6, 6, 8, 8]
-dice2 = [2, 2, 4, 4, 9, 9]
-# Expect 16, 20
-print(count_wins(dice1, dice2)) 
+# dice1 = [1, 1, 6, 6, 8, 8]
+# dice2 = [2, 2, 4, 4, 9, 9]
+# # Expect 16, 20
+# print(count_wins(dice1, dice2)) 
+
+def find_the_best_dice(dices):
+    assert all(len(dice) == 6 for dice in dices)
+    size = len(dices)
+    # Empty list to store win time of each dice
+    count = [0] * size
+    for i in range(0, size):
+      for j in range(i+1, size):  
+        result_wini, result_winj =  count_wins(dices[i], dices[j])
+        if result_wini > result_winj:
+          count[i] += 1
+        elif result_wini < result_winj:
+          count[j] += 1    
+    # Check if best dice exits
+    # Return index of best dice
+    for i in range(0, size):
+      if count[i] == size - 1:
+        return i
+    # No best dice
+    return -1
+
